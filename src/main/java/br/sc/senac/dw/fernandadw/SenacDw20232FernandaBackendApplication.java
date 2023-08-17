956b277cfd06a441c7d822ce299568d4357c970c
fernanda da lapa <fernandadalapa@hotmail.com>
2023-08-17 10:49:34 -0300

    Primeiro commit

---
+++ src/main/java/br/sc/senac/dw/fernandadw/SenacDw20232FernandaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw.fernandadw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacDw20232FernandaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacDw20232FernandaBackendApplication.class, args);
+	}
+
+}
