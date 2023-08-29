99a054b580fda2dc3124018313af23664f53f6f9
fernanda da lapa <fernandadalapa@hotmail.com>
2023-08-29 10:48:18 -0300

    Modificações

--- src/main/java/br/sc/senac/dw/fernandadw/ProdutoRepository.java
+++ src/main/java/br/sc/senac/dw/fernandadw/ProdutoRepository.java
@@ -1,14 +1,14 @@
 package br.sc.senac.dw.fernandadw;
 
-public class ProdutoRepository {
-	
-	@Repository
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import br.sc.senac.dw.fernandadw.controller.Produto;
+
+@Repository
 	public interface ProdutoRepository extends JpaRepository<Produto, Long>, JpaSpecificationExecutor<Produto> {
-		//É possível declarar métodos do JpaRepository aqui, combinando atributos e operadores SQL
-		
-		//https://reflectoring.io/spring-data-specifications/
-		//Exemplo
 		List<Produto> findAllByNomeLike(String nomeInformado);
 	}
-
-}
