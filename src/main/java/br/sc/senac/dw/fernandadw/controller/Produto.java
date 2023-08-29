2ce8150d89a0792faf04430fc458110cb4b3f479
fernanda da lapa <fernandadalapa@hotmail.com>
2023-08-29 10:36:44 -0300

    Alterações.

---
+++ src/main/java/br/sc/senac/dw/fernandadw/controller/Produto.java
@@ -0,0 +1,95 @@
+package br.sc.senac.dw.fernandadw.controller;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name = "id_fabricante")
+	private Fabricante fabricante;
+	private Double valor;
+	private Double peso;
+	private LocalDate dataCadastro;
+	
+	public Produto(Integer id, String nome, Fabricante fabricante, Double valor, Double peso, LocalDate dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.valor = valor;
+		this.peso = peso;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Produto() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto id: " + id 
+				+ "\nNome: " + nome 
+				+ "\nValor: " + valor 
+				+ "\nPeso: " + peso 
+				+ "\nDataCadastro: " + dataCadastro;
+	}
+}
--- src/main/java/br/sc/senac/dw/fernandadw/controller/ProdutoController.java
+++ src/main/java/br/sc/senac/dw/fernandadw/controller/ProdutoController.java
@@ -3,7 +3,12 @@ package br.sc.senac.dw.fernandadw.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,33 @@ public class ProdutoController {
 	public List<Produto> listarTodos() {
 		return produtoService.listarTodos();
 	}
+
+
+	@PostMapping("/filtro")
+	public List<Produto> listarComSeletor(@RequestBody ProdutoSeletor seletor){
+		return produtoService.listarComSeletor(seletor);
+	}
+
+	@GetMapping("/{id}")
+	public Produto pesquisarPorId(@PathVariable Integer id){
+		return produtoService.consultarPorId(id.longValue());
+	}
+	
+	@PostMapping
+	public Produto salvar(@RequestBody Produto novoProduto) 
+			throws CampoInvalidoException {
+		return produtoService.inserir(novoProduto);
+	}
+	
+	@PutMapping()
+	public boolean atualizar(@RequestBody Produto produtoParaAtualizar) 
+			throws CampoInvalidoException {
+		return produtoService.atualizar(produtoParaAtualizar) != null;
+	}
+	
+	@DeleteMapping("/{id}")
+	public boolean excluir(@PathVariable Integer id) {
+		return produtoService.excluir(id);
+	}
+
 }
