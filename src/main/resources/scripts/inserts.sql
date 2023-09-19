c15371b188b76cc24206667b515fc3d718f2a8d2
fernanda da lapa <fernandadalapa@hotmail.com>
2023-09-19 09:50:35 -0300

    Prova 1

--- src/main/resources/scripts/inserts.sql
+++ src/main/resources/scripts/inserts.sql
@@ -1,14 +1,14 @@
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('3 corações', '11222333000022');
+INSERT INTO exemplos.fabricantes (nome, cnpj, cidade, uf)
+VALUES('3 corações', '11222333000022', 'São José', 'SC');
 
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Melita', '10222333000022');
+INSERT INTO exemplos.fabricantes (nome, cnpj, cidade, uf)
+VALUES('Melita', '10222333000022', 'Florianópolis', 'SC');
 
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Italac', '55222333000022');
+INSERT INTO exemplos.fabricantes (nome, cnpj, cidade, uf)
+VALUES('Italac', '55222333000022', 'Palhoça', 'SC');
 
-INSERT INTO exemplos.fabricantes (nome, cnpj)
-VALUES('Tirol', '88222333000022');
+INSERT INTO exemplos.fabricantes (nome, cnpj, cidade, uf)
+VALUES('Tirol', '88222333000022', 'Pomerode', 'SC');
 
 INSERT INTO exemplos.produtos (nome, id_fabricante, valor, peso, data_cadastro)
 VALUES('Café', 1, 12.5, 0.5, sysdate());
